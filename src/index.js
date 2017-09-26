60534c9189402b04680322594e1a4820ec629858
Issue <zhushisong@skio.com>
2017-09-26 14:04:17 +0800

    remove reducer

--- src/index.js
+++ src/index.js
@@ -2,14 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import App from './App';
-import configureStore from './configureStore';
 import './index.css';
 
-const store = configureStore();
 
 ReactDOM.render(
-  <Provider store={store}>
-      <App />
-  </Provider>,
+  <App />,
   document.getElementById('root')
 );
