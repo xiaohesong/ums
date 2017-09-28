7c4f4fa81ea4e3e5344f1262b6c0bad9727bf42b
Issue <zhushisong@skio.com>
2017-09-28 09:42:18 +0800

    点击个人信息

--- src/components/LocalTime.js
+++ src/components/LocalTime.js
@@ -39,13 +39,13 @@ export default class LocalTime extends Component {
               <li className="skio-dropdown-user-info">
                   <div className='skio-dropdown-toggle'>
                     <a className="skio-dropdown-toggle" style={{cursor: 'pointer'}} onClick={this.toggleUserInfo} >
-                        Hi，<span className="skio-username">user-name</span>
+                        Hi，<span className="skio-username">管理员</span>
                       <span className="skio-down-icon"><Icon type="down" /></span>
                     </a>
                   </div>
                   <ul className="dropdown-menu" style={{display: this.state.display ? "block" : "none"}}>
-                      <h3 className="ng-binding">user-name</h3>
-                      <p className="company ng-binding">hanngzhougongsi</p>
+                      <h3 className="ng-binding">管理员</h3>
+                      <p className="company ng-binding">杭州市西湖区北山大厦</p>
                       <hr style={{margin: "15 0"}} />
                       <p className="phone">手机号<span className="ng-binding">15088614450</span></p>
                       <p className="manage-user">管理员<span className="ng-binding">xiaofan</span></p>
