9a81aecfb420fb069bba74a98783dcf020ba1bb9
xiaohesong <didmehh@163.com>
2018-04-17 16:13:53 +0800

    base64 remote image

---
+++ src/until/Base64RemoteImg.js
@@ -0,0 +1,15 @@
+// 这里这个是将远程的图片转换成base64。 非canvas情况.
+
+export function base64Url(url, callback) {
+  let xhr = new XMLHttpRequest();
+  xhr.onload = function() {
+    let reader = new FileReader();
+    reader.onloadend = function() {
+      callback(reader.result);
+    }
+    reader.readAsDataURL(xhr.response);
+  };
+  xhr.open('GET', url);
+  xhr.responseType = 'blob';
+  xhr.send();
+}
